0ebf88eb407a55dbcdd211aa4ae61c6296672e83
Zeki411 <nguyenhatrung411@gmail.com>
2024-05-18 16:12:50 +0000

    up urdf

--- zbot_franka_description/launch/description.launch.py
+++ zbot_franka_description/launch/description.launch.py
@@ -6,15 +6,12 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    load_gripper_declare = DeclareLaunchArgument('load_gripper', default_value='true', description='Use Franka Gripper as end-effector if true. Robot is loaded without end-effector otherwise')
-
-    load_gripper = LaunchConfiguration('load_gripper')
 
     zbot_franka_xacro_file = os.path.join(get_package_share_directory('zbot_franka_description'), 'urdf',
-                                     'panda.urdf.xacro')
+                                     'panda_main.urdf.xacro')
     
     robot_description_content = Command(
-        [FindExecutable(name='xacro'), ' ', zbot_franka_xacro_file, ' hand:=', load_gripper])
+        [FindExecutable(name='xacro'), ' ', zbot_franka_xacro_file])
     
     # Static TF
     static_tf = Node(
@@ -35,7 +32,6 @@ def generate_launch_description():
     
     ld = LaunchDescription()
 
-    ld.add_action(load_gripper_declare)
     ld.add_action(robot_state_publisher_node)
     # ld.add_action(static_tf)
 
